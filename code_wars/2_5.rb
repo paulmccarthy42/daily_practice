ef948c1919f3bdc5184792a708ed1c377d9a5fd8
Paul McCarthy <phantasos42@gmail.com>
2018-02-05 23:07:26 -0600

    remainder of code wars 2/5

--- code_wars/2_5.rb
+++ code_wars/2_5.rb
@@ -13,7 +13,7 @@ def sum_two_smallest_numbers(numbers)
   numbers.each do |x|
     if second_smallest == nil || x < second_smallest
       if smallest == nil || x < smallest
-      second_smallest = smallest
+        second_smallest = smallest
         smallest = x
       else
         second_smallest = x
@@ -22,3 +22,55 @@ def sum_two_smallest_numbers(numbers)
   end
   return smallest + second_smallest
 end
+
+# x Simple, given a string of words, return the length of the shortest word(s).
+
+# String will never be empty and you do not need to account for different data types.
+
+def find_short(s)
+  words = s.split(" ")
+  l = nil
+  words.each do |x|
+    if l == nil || x.length < l
+      l = x.length
+    end
+  end
+  return l # l: length of the shortest word
+end
+
+# The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+# Examples:
+
+# "din" => "((("
+
+# "recede" => "()()()"
+
+# "Success" => ")())())"
+
+# "(( @" => "))(("
+
+def duplicate_encode(word)
+  letter_count = Hash.new(0)
+  word.each_char {|letter| letter_count[letter.downcase] += 1}
+  encoded = ""
+  word.each_char do |letter| 
+    encoded += "(" if letter_count[letter.downcase] == 1
+    encoded += ")" if letter_count[letter.downcase] > 1
+  end
+  return encoded
+end
+
+# Create a function named divisors/Divisors that takes an integer and returns an array with all of the integer's divisors(except for 1 and the number itself). If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+# Example:
+# divisors(12) # should return [2,3,4,6]
+# divisors(25) # should return [5]
+# divisors(13) # should return "13 is prime"
+# You can assume that you will only get positive integers as inputs.
+
+def divisors(n)
+  divisors = (2..n/2).select {|x| n % x == 0}
+  return divisors == [] ? "#{n} is prime" : divisors 
+end
+
