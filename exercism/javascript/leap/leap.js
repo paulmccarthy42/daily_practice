ff00d0ef9b892e22f4c8950b045f42f33e322f49
Paul McCarthy <phantasos42@gmail.com>
2018-03-04 17:26:57 -0600

    javascript start

---
+++ exercism/javascript/leap/leap.js
@@ -0,0 +1,25 @@
+//
+// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Year = function() {
+  //
+  // YOUR CODE GOES HERE
+  //
+};
+
+Year.prototype.isLeap = function() {
+  //
+  // YOUR CODE GOES HERE
+  //
+};
+
+module.exports = Year;
+
+function foo() {
+  console.log("hello");
+}
+
+let x = foo();
+x();
