7765899fbd3301675e78a763d0379927f7149f7c
Paul McCarthy <phantasos42@gmail.com>
2018-03-05 20:44:47 -0600

    tdd error handling

--- tdd_practice/bank_account.rb
+++ tdd_practice/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
-
   attr_reader :balance
 
   def initialize(starting_balance=0)
+    raise ArgumentError if !starting_balance.is_a?(Integer)
     @balance = starting_balance.to_i
   end
 
@@ -11,20 +11,22 @@ class BankAccount
   end
 
   def deposit(amount)
+    raise ArgumentError if !amount.is_a?(Integer)
     @balance += amount
   end
 
   def withdraw(amount)
+    raise ArgumentError if !amount.is_a?(Integer)
     if (@balance - amount) > 0
       @balance -= amount
     end
   end
 
   def transfer_to(other_account, amount)
+    raise ArgumentError if !amount.is_a?(Integer) || !other_account.is_a?(BankAccount)
     if (@balance - amount) > 0
       withdraw(amount)
       other_account.deposit(amount)
     end
   end
-
 end
--- tdd_practice/bank_account_test.rb
+++ tdd_practice/bank_account_test.rb
@@ -7,14 +7,8 @@ class BankAccountTest < MiniTest::Unit::TestCase
     assert_equal "Your balance is $5", account.check_balance
   end
 
-  def test_check_balance_with_valid_balance_in_wrong_format
-    account = BankAccount.new("5")
-    assert_equal "Your balance is $5", account.check_balance
-  end
-
   def test_check_balance_with_invalid_balance
-    account = BankAccount.new("hello")
-    assert_equal "Your balance is $0", account.check_balance
+    assert_raises(ArgumentError) { test = BankAccount.new("hello") }
   end
 
   def test_math_around_deposits
@@ -25,6 +19,11 @@ class BankAccountTest < MiniTest::Unit::TestCase
     assert_equal add + base, account.balance
   end
 
+  def test_invalid_deposit
+    account = BankAccount.new(5)
+    assert_raises(ArgumentError) { account.deposit("hello")}
+  end
+
   def test_math_around_withdrawals
     base = 100
     withdrawal = rand(99)
@@ -33,11 +32,41 @@ class BankAccountTest < MiniTest::Unit::TestCase
     assert_equal base - withdrawal, account.balance
   end
 
-  def test_overdrafting
+  def test_overdrafting_on_withdrawal
     base = rand(100)
     withdrawal = base + rand(50)
     account = BankAccount.new(base)
     account.withdraw(withdrawal)
     assert_equal base, account.balance
+  end
+
+  def test_invalid_withdrawal
+    account = BankAccount.new(5)
+    assert_raises(ArgumentError) { account.withdraw("hello")}
+  end
+
+  def test_transfer
+    account_1 = BankAccount.new(100)
+    account_2 = BankAccount.new(100)
+    account_1.transfer_to(account_2, 50)
+    assert_equal [50, 150], [account_1.balance, account_2.balance]
+  end
+
+  def test_overdraft_on_transfer
+    account_1 = BankAccount.new(100)
+    account_2 = BankAccount.new(100)
+    account_1.transfer_to(account_2, 150)
+    assert_equal [100, 100], [account_1.balance, account_2.balance]
+  end
+
+  def test_invalid_account_for_transfer
+    account_1 = BankAccount.new(100)
+    assert_raises(ArgumentError) { account_1.transfer_to("hello", 150) }
+  end
+
+  def test_invalid_amount_for_transfer
+    account_1 = BankAccount.new(100)
+    account_2 = BankAccount.new(100)
+    assert_raises(ArgumentError) { account_1.transfer_to(account_2, "hello") }
   end
 end
