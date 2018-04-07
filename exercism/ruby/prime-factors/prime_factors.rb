843a1748e0e37a35263c3a952a3edf8ffa6845d8
Paul McCarthy <phantasos42@gmail.com>
2018-04-06 22:27:38 -0500

    use prime module

--- exercism/ruby/prime-factors/prime_factors.rb
+++ exercism/ruby/prime-factors/prime_factors.rb
@@ -1,27 +1,22 @@
+require 'prime' # depending on use, it would probably be worth building this locally,
+# but given how high the numbers are in the tests, I couldn't get the tests to
+# run in a reasonable time, so using this
+
 class PrimeFactors
   def self.for(number)
     factors = []
     while number > 1
       next_factor = self.lowest_prime_factor(number)
-      factors << next_factor
-      number /= next_factor
+      factors << next_factor && number /= next_factor if next_factor
     end
     factors
   end
 
   def self.lowest_prime_factor(number)
-    primes.each do |prime|
-      if number % prime == 0
-        return prime
-      end
+    Prime.each(number) do |prime|
+      return prime if number % prime == 0
     end
-    
   end
 
-  def self.primes
-    [2,3,5,7,11,13,17,23,461,9539,894119]
-  end
+  # ffr, this is pretty easily refactorable to be recursive
 end
-
-p PrimeFactors.for(4)
-
