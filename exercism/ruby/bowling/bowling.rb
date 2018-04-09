17d330a2eea82dea8b43c1c33dada336c7936574
Paul McCarthy <phantasos42@gmail.com>
2018-04-08 22:00:17 -0500

    refactor to use frames over rolls, and start error reporting

--- exercism/ruby/bowling/bowling.rb
+++ exercism/ruby/bowling/bowling.rb
@@ -1,44 +1,64 @@
 class Game
-  attr_accessor :score, :rolls
+  attr_accessor :rolls, :frames #for testing
+
+  BowlingError = "Failed the rules of bowling"
 
   def initialize
     @score = 0
     @rolls = []
-    @frame = 0
+    @frames = [[]]
+  end
+
+  def score
+    raise BowlingError if @frames.length < 10
+    @score
   end
 
   def spare?
-    @rolls.length > 1 && 
-    @rolls[-1] + @rolls[-2] == 10 &&
-    @rolls[-1] != 10 &&
-    @rolls[-2] != 10
+    @frames.last.sum == 10 && !@frames.last.include?(10) && @frames.length < 10
   end
 
   def new_strike?
-    @rolls.length >= 1 && @rolls[-1] == 10
+    @frames.last.include?(10) && @frames.length < 10
   end
 
   def old_strike?
-    (@rolls.length >= 2 && @rolls[-2] == 10)
+    (@rolls.length >= 2 && @rolls[-2] == 10) && @frames.length < 10
+  end
+
+  def frame_incomplete?
+    @frames.last.last != 10 && @frames.last.length < 2
+  end
+
+  def wrong_number_of_pins?(pins)
+    !pins.between?(0,10) ||
+    (frame_incomplete? && 
+      @frames.last.last && 
+      @frames.last.last + pins > 10)
+  end
+
+  def cant_roll?(x)
+    wrong_number_of_pins?(x)
+
   end
 
   def roll(pins)
-    @score += pins if @rolls.length < 20
+    raise BowlingError if cant_roll?(pins)
     @score += pins if spare?
     @score += pins if new_strike?
     @score += pins if old_strike?
+
+    @score += pins
+
     @rolls << pins
-    @frame += 1 if @rolls.length.even?
+    if frame_incomplete?
+      @frames.last << pins
+    else
+      @frames << [pins]
+    end
   end
 end
 
 module BookKeeping
   VERSION = 3
 end
-
-# x = Game.new
-# x.roll(10)
-# x.roll(10)
-# x.roll(5)
-# p x.score
-# p x.rolls
