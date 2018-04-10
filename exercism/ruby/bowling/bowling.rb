2f157ecca75af72d756dc95406636f07b9578bff
Paul McCarthy <phantasos42@gmail.com>
2018-04-09 23:17:09 -0500

    another shot at bowling

--- exercism/ruby/bowling/bowling.rb
+++ exercism/ruby/bowling/bowling.rb
@@ -15,15 +15,21 @@ class Game
   end
 
   def spare?
-    @frames.last.sum == 10 && !@frames.last.include?(10) && @frames.length < 10
+    @frames[-2] &&
+      @frames[-2].sum == 10 && 
+      !@frames[-2].include?(10) && 
+      @frames.length <= 10 &&
+      @frames.last.length == 1
   end
 
   def new_strike?
-    @frames.last.include?(10) && @frames.length < 10
+    @frames[-2] &&
+      @frames[-2].include?(10) && 
+      @frames.length <= 10
   end
 
   def old_strike?
-    (@rolls.length >= 2 && @rolls[-2] == 10) && @frames.length < 10
+    (@rolls.length >= 2 && @rolls[-2] == 10) && @frames.length <= 10
   end
 
   def frame_incomplete?
@@ -37,28 +43,45 @@ class Game
       @frames.last.last + pins > 10)
   end
 
+  def game_over?
+    @frames.length >= 10 &&
+    !frame_incomplete? &&
+    !spare? &&
+    !new_strike? &&
+    !old_strike?
+  end
+
   def cant_roll?(x)
-    wrong_number_of_pins?(x)
+    wrong_number_of_pins?(x) ||
+    game_over?
 
   end
 
   def roll(pins)
     raise BowlingError if cant_roll?(pins)
-    @score += pins if spare?
-    @score += pins if new_strike?
-    @score += pins if old_strike?
-
-    @score += pins
-
     @rolls << pins
     if frame_incomplete?
       @frames.last << pins
     else
       @frames << [pins]
     end
+    @score += pins if spare?
+    @score += pins if new_strike?
+    @score += pins if old_strike?
+
+    @score += pins
+
   end
 end
 
 module BookKeeping
   VERSION = 3
 end
+
+x = Game.new
+y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3]
+y.each {|z| x.roll(z)}
+p x.frames
+p x.score
+p x.game_over?
+p x.new_strike?
